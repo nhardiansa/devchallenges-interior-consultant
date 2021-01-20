a9c7d43af983c365b171d2499a52d2d3f0be8fe4
nhardiansa <nhardiansa@gmail.com>
2021-01-20 19:25:35 +0800

    memperbaiki tampilan navigasi pada mobile

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,13 @@ const navigation = document.querySelector("nav .menu");
 const closeMenu = document.querySelector("nav .menu span")
 
 hamburger.addEventListener("click", () => {
-    navigation.classList.toggle("show")
+    navigation.classList.toggle("hidden")
     console.log("hi");
     
 })
 
 closeMenu.addEventListener("click", () => {
-    navigation.classList.toggle("show")
+    navigation.classList.toggle("hidden")
     console.log("ho");
     
 })
